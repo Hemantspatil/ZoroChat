f2acaf89f9951fe39a6b5353686b54c65ae58996
Hemantspatil <hemantapatila111@gmail.com>
2025-06-21 15:56:34 +0530

    created the SignUpPage.jsx

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
 import cookieparser from 'cookie-parser'
+import cors from 'cors'
+
 
 
 import { connectDB } from './lib/db.js';
@@ -15,6 +17,10 @@ dotenv.config();
 const PORT  = process.env.PORT;
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
