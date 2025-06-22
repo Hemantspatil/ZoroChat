1ae121a4f361d96262217e89e366c3e30ee04d79
Hemantspatil <hemantapatila111@gmail.com>
2025-06-23 01:17:08 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import cookieparser from 'cookie-parser'
 import cors from 'cors'
 
-
+import path from 'path'
 
 import { connectDB } from './lib/db.js';
 import { app, server } from './lib/socket.js';
@@ -16,7 +16,8 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT  = process.env.PORT;
-// app.use(express.json());
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieparser());
 app.use(cors({
@@ -27,7 +28,14 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+
+}
 
 
 server.listen(PORT, () => {
