5ce3afe44106196c72f0e4009831f2f9813f3bd3
Hemantspatil <hemantapatila111@gmail.com>
2025-06-23 12:25:51 +0530

    Path : TypeError Fix 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    if (req.originalUrl.startsWith("/api")) return; // Prevent sending index.html for API routes
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
