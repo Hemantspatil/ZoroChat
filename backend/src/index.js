717a3dc3b9607ed7a00f19e16cf36e07083fdef1
Hemantspatil <hemantapatila111@gmail.com>
2025-06-23 10:33:14 +0530

    trying

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,12 +39,11 @@ if(process.env.NODE_ENV === "production"){
 
 await connectDB();;
 
-if (process.env.NODE_ENV !== "production") {
+
     server.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`); 
         connectDB();
     });
-}
 
 
-export default server;
+
